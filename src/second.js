c2f87011b9d0eae1ffe06594528b5fb109dd00a0
yenifer alexandra Galarza pezo <yeniferalexandra2000@hotmail.com>
2019-11-19 17:45:02 -0500

    cambios de plussss

--- src/second.js
+++ src/second.js
@@ -1,30 +1,42 @@
-const hideOrShow = (iddepregunt) => {
+const algo = document.querySelectorAll("div.plusOrMin");
+algo.forEach(function(userItem) {
+    userItem.innerHTML = `     <i class="fas fa-plus p_5"></i>`;
+});
+
+
+
+
+const hideOrShow = (iddepregunt, idplus) => {
     const element = document.getElementById(iddepregunt);
     element.classList.toggle("blockPreg");
+    const plusornot = document.getElementById(idplus);
+    plusornot.innerHTML = "";
+    plusornot.innerHTML = `<i class="fas fa-minus p_5"></i>`;
 }
 
-document.getElementById("preg1").addEventListener("click", () => { hideOrShow("cont-preg1") });
 
-document.getElementById("preg2").addEventListener("click", () => { hideOrShow("cont-preg2") });
-document.getElementById("preg3").addEventListener("click", () => { hideOrShow("cont-preg3") });
-document.getElementById("preg4").addEventListener("click", () => { hideOrShow("cont-preg4") });
-document.getElementById("preg5").addEventListener("click", () => { hideOrShow("cont-preg5") });
-document.getElementById("preg6").addEventListener("click", () => { hideOrShow("cont-preg6") });
-document.getElementById("preg7").addEventListener("click", () => { hideOrShow("cont-preg7") });
-document.getElementById("preg8").addEventListener("click", () => { hideOrShow("cont-preg8") });
-document.getElementById("preg9").addEventListener("click", () => { hideOrShow("cont-preg9") });
-document.getElementById("preg10").addEventListener("click", () => { hideOrShow("cont-preg10") });
-document.getElementById("preg11").addEventListener("click", () => { hideOrShow("cont-preg11") });
-document.getElementById("preg12").addEventListener("click", () => { hideOrShow("cont-preg12") });
-document.getElementById("preg13").addEventListener("click", () => { hideOrShow("cont-preg13") });
-document.getElementById("preg14").addEventListener("click", () => { hideOrShow("cont-preg14") });
-document.getElementById("preg15").addEventListener("click", () => { hideOrShow("cont-preg15") });
-document.getElementById("preg16").addEventListener("click", () => { hideOrShow("cont-preg16") });
-document.getElementById("preg17").addEventListener("click", () => { hideOrShow("cont-preg17") });
-document.getElementById("preg18").addEventListener("click", () => { hideOrShow("cont-preg18") });
-document.getElementById("preg19").addEventListener("click", () => { hideOrShow("cont-preg19") });
+document.getElementById("preg1").addEventListener("click", () => { hideOrShow("cont-preg1", "sig1") });
 
-document.getElementById("preg20").addEventListener("click", () => { hideOrShow("cont-preg20") });
+document.getElementById("preg2").addEventListener("click", () => { hideOrShow("cont-preg2", "sig2") });
+document.getElementById("preg3").addEventListener("click", () => { hideOrShow("cont-preg3", "sig3") });
+document.getElementById("preg4").addEventListener("click", () => { hideOrShow("cont-preg4", "sig4") });
+document.getElementById("preg5").addEventListener("click", () => { hideOrShow("cont-preg5", "sig5") });
+document.getElementById("preg6").addEventListener("click", () => { hideOrShow("cont-preg6", "sig6") });
+document.getElementById("preg7").addEventListener("click", () => { hideOrShow("cont-preg7", "sig7") });
+document.getElementById("preg8").addEventListener("click", () => { hideOrShow("cont-preg8", "sig8") });
+document.getElementById("preg9").addEventListener("click", () => { hideOrShow("cont-preg9", "sig9") });
+document.getElementById("preg10").addEventListener("click", () => { hideOrShow("cont-preg10", "sig10") });
+document.getElementById("preg11").addEventListener("click", () => { hideOrShow("cont-preg11", "sig11") });
+document.getElementById("preg12").addEventListener("click", () => { hideOrShow("cont-preg12", "sig12") });
+document.getElementById("preg13").addEventListener("click", () => { hideOrShow("cont-preg13", "sig13") });
+document.getElementById("preg14").addEventListener("click", () => { hideOrShow("cont-preg14", "sig14") });
+document.getElementById("preg15").addEventListener("click", () => { hideOrShow("cont-preg15", "sig15") });
+document.getElementById("preg16").addEventListener("click", () => { hideOrShow("cont-preg16", "sig16") });
+document.getElementById("preg17").addEventListener("click", () => { hideOrShow("cont-preg17", "sig17") });
+document.getElementById("preg18").addEventListener("click", () => { hideOrShow("cont-preg18", "sig18") });
+document.getElementById("preg19").addEventListener("click", () => { hideOrShow("cont-preg19", "sig19") });
+
+document.getElementById("preg20").addEventListener("click", () => { hideOrShow("cont-preg20", "sig20") });
 const elem = document.getElementById("LigoContainer");
 const elem2 = document.getElementById("ligoPlusContainer");
 
